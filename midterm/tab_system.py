c9394dfcc62d651eaf02a0e6c70aa38063aa8302
ALichrieff <alichrief12gmail.com>
2023-11-10 09:48:06 +0200

    Created a function to add a tab to a list by defining its title and url

--- midterm/tab_system.py
+++ midterm/tab_system.py
@@ -1,10 +1,23 @@
+tabs = []
+def open_tab(web_title,web_url):
+  tabs.append({"title":web_title,"url":web_url})
+  print(tabs)
+
+
+
 def main():
- print("""1. Open Tab
-          2. Close Tab
-          3. Switch Tab
-          4. Display All Tabs
-          5. Open Nested Tab
-          6. Clear All Tabs
-          7. Save Tabs
-          8. Import Tabs
-          9. Exit""")
+  print("""
+           1. Open Tab
+           2. Close Tab
+           3. Switch Tab
+           4. Display All Tabs
+           5. Open Nested Tab
+           6. Clear All Tabs
+           7. Save Tabs
+           8. Import Tabs
+           9. Exit
+        """)
+  
+
+while True:
+  main()
