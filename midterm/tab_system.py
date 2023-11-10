ddb91294b5deb43efa55d9ef40cdb1a2e5e64575
ALichrieff <alichrief12gmail.com>
2023-11-10 11:13:12 +0200

    Handle the user input if enter nothing

--- midterm/tab_system.py
+++ midterm/tab_system.py
@@ -4,7 +4,11 @@ def open_tab(web_title,web_url):
   tabs.append({"title":web_title,"url":web_url})
   print(tabs)
 
+def close_tab(index):
   
+
+
+
 
 def main():
   print("""
@@ -24,8 +28,13 @@ def main():
     url = input("Enter the url of your website :")
     open_tab(title,url)
   if user_choice == 2:
-    user_index = int(input("Enter the index"))
-    close_tab(user_index)
+    user_index = input("Enter the index of tab you want to remove :")
+    if(user_index):
+     user_index = int(user_index)
+     close_tab(user_index)
+    else:
+      close_tab(None)
+
 
 while True:
   main()
